255a15f5326f9a2efa79133a4c9cf0fea8c58ddc
vickyvasu <vickyvasu6767@gmail.com>
2026-04-16 06:50:51 +0530

    update backend API URL in api.js

---
+++ client/src/config.js
@@ -0,0 +1,5 @@
+const config = {
+  API_BASE_URL: "https://mern-stack-project-2-bwuo.onrender.com"
+};
+
+export default config;
--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://mern-stack-project-3-z5jd.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
